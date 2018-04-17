07d0d101eee5c9a89a81293f03a01a879ec1ee5d
sjohn0731 <34875798+sjohn0731@users.noreply.github.com>
2018-04-16 20:49:59 -0700

    Add files via upload
    added comments for code clarity; was going to add map display but deleted it when Ryan found better and more responsive coding

---
+++ Project1-master/assets/javascript/style.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+   
+    window.initMap = function(){
+        var losangeles = {lat: 34.07, lng: -118.24};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 14,
+          center: losangeles
+        });
+        var marker = new google.maps.Marker({
+          position: losangeles,
+          map: map
+        });
+      }
+    });
+
+
+
+
