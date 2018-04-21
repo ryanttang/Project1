b77fbd4438e9c27ecacb7d421115549ecc2c31be
rttang <rttang@admins-MacBook-Pro.local>
2018-04-20 18:52:39 -0700

    More Calendar testing

--- assets/javascript/calendar.js
+++ assets/javascript/calendar.js
@@ -97,7 +97,7 @@
           'orderBy': 'startTime'
         }).then(function(response) {
           var events = response.result.items;
-          appendPre('Upcoming events:');
+          appendPre('Upcoming Events:');
 
           if (events.length > 0) {
             for (i = 0; i < events.length; i++) {
@@ -114,3 +114,71 @@
         });
       }
 
+      // Google api console clientID and apiKey 
+
+      var clientId = '463672788287-b7nicgrv2ldodtgq79vj7rul8615g31g.apps.googleusercontent.com';
+      var apiKey = 'AIzaSyDK5f1arv2v9X0DsiDMCQYe7DxElk_ivRc';
+     
+      // enter the scope of current project (this API must be turned on in the Google console)
+        var scopes = 'https://www.googleapis.com/auth/calendar';
+     
+     
+     // OAuth2 functions
+          function handleClientLoad() {
+                gapi.client.setApiKey(apiKey);
+                window.setTimeout(checkAuth, 1);
+             }
+     
+     //To authenticate
+       function checkAuth() {
+         gapi.auth.authorize({ client_id: clientId, scope: scopes, immediate: true }, handleAuthResult);
+             }
+     
+     // This is the resource we will pass while calling api function
+     var resource = {
+                 "summary": "My Event",
+                 "start": {
+                     "dateTime": today
+                 },
+                 "end": {
+                     "dateTime": twoHoursLater
+                 },
+                 "description":"We are organizing events",
+                 "location":"US",
+                 "attendees":[
+                 {
+                         "email":"attendee1@gmail.com",
+                         "displayName":"Jhon",
+                         "organizer":true,
+                         "self":false,
+                         "resource":false,
+                         "optional":false,
+                         "responseStatus":"needsAction",
+                         "comment":"This is my demo event",
+                         "additionalGuests":3
+                         
+                 },
+                 {    
+                     "email":"attendee2@gmail.com",
+                         "displayName":"Marry",
+                         "organizer":true,
+                         "self":false,
+                         "resource":false,
+                         "optional":false,
+                         "responseStatus":"needsAction",
+                         "comment":"This is an official event",
+                         "additionalGuests":3
+                 }
+                 ],
+             };
+     
+     function makeApiCall(){
+     gapi.client.load('calendar', 'v3', function () { // load the calendar api (version 3)
+                     var request = gapi.client.calendar.events.insert
+                     ({
+                         'calendarId': 'teamjotproject@gmail.com', 
+     // calendar ID which id of Google Calendar where you are creating events. this can be copied from your Google Calendar user view.
+     
+                         "resource": resource 	// above resource will be passed here
+                     });                
+     }
